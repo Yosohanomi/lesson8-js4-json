d5518d818dc13a34474301aa5738ed8dfe07bcee
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2025-10-28 18:43:32 +0200

    json

---
+++ src/script.js
@@ -0,0 +1,15 @@
+// import userData from "./data/products.json";
+
+// console.log(userData);
+
+// const {user} = userData;
+// console.log(user);
+
+// const {products} = userData;
+// console.log(products);
+
+import {user, products, favoriteProducts} from "./data/products.json";
+
+console.log(user);
+console.log(products);
+console.log(favoriteProducts);
