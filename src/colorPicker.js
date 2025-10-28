19c22ffd3b5fb995ca8e17ba50465736869883a0
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2025-10-28 20:08:35 +0200

    ++

--- src/colorPicker.js
+++ src/colorPicker.js
@@ -4,3 +4,14 @@ const palleteContainer = document.querySelector(".js-palette");
 
 const layout = cardsTemplate(colors);
 palleteContainer.innerHTML = layout;
+
+palleteContainer.addEventListener('click', onColorClick);
+
+function onColorClick(event) {
+    if (!event.target.classList.contains('color-swatch')) {
+        return;
+    }
+    const hexColor = event.target.dataset.hex;
+    const rgbColor = event.target.dataset.rgb;
+    document.body.style.backgroundColor = hexColor;
+}
